97371e9dab3cd7ef6685680a919a98b2bfc99e14
zenathar@gmail.com <zenathar@gmail.com@859ed2cb-8662-89ad-f123-0842d5b97d34>
2012-10-08 22:23:23 +0000

    Działająca wektoryzacja i dewektoryzacja bloków

--- trunk/robust/blok.cpp
+++ trunk/robust/blok.cpp
@@ -10,7 +10,7 @@ void blok::dump(void){
 		{
 			if(i%size==0)
 				cout<<endl;
-			cout<<content.at(i);
+			cout<<content.at(i)<<" ";
 		}
 }
 
@@ -18,15 +18,21 @@ blok::blok(Mat input_block, int _size){
 	unsigned char *input = (unsigned char*)(input_block.data);
 	for(int i=0;i<size;i++)
 		for(int j=0;j<size;j++)
-		content.push_back(input[input_block.step1()*i+j]); //wywala program
+		content.push_back(input[input_block.step1()*i+j]); 
 	weight=0;
 	size=_size;
 }
 int blok::operator()(int x, int y){
 	return content.at(x+y*size);
 }
-Mat& blok::operator=(blok&){
-	Mat tmp;
-	tmp.create(size,size,CV_8UC1);
-	return tmp;
+Mat& blok::devectorize(){ //TODO: DOPISAC OBSLUGE WYJATKU GDY ROZMIARY CONTENT I MAT SIE NIE ZGADZAJA
+	Mat *tmp=new Mat(size,size,CV_8UC1);
+	
+	for(int i=0;i<size;i++)
+		for(int j=0;j<size;j++)
+			tmp->at<uchar>(i,j) = content.at(i*size+j);
+	return *tmp;
+}
+int blok::getSize(){
+return content.size();
 }
