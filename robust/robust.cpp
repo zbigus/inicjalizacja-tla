b42379e7cb6289d63d689a828f997e331b0f0b93
zenathar <zenathar@gmail.com>
2012-10-08 17:50:58 +0000

    Podzielono projekt na mniejsze kawałki

--- robust/robust.cpp
+++ robust/robust.cpp
@@ -9,6 +9,7 @@
 #include <iomanip>
 #include <deque>
 #include <math.h>
+#include "utils.h"
 using namespace cv;
 using namespace std;
 
@@ -21,23 +22,7 @@ int movie_width;
 int movie_height;
 //Matqueue** history;
 
-Mat** grid_cut(Mat input_image, int size) {
-	int height = (int) input_image.rows / size;
-	int width = (int) input_image.cols / size;
-	Mat** Container = new Mat *[height];
-	for (int i = 0; i < height; i++)
-		Container[i] = new Mat[width];
-	Rect roi;
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			roi = Rect(j * size, i * size, size, size);
-			Mat tmp_mat = Mat(input_image, roi);
-			Container[i][j] = tmp_mat;
-		}
-	}
 
-	return Container;
-}
 
 /*Matqueue** createHistory(int height, int width) {
 	Matqueue** History = new Matqueue *[height];
