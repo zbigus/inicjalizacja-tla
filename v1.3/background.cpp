8b41c26c01b1dd80cf772a6a042825b38e290ca8
zenathar <zenathar@gmail.com>
2012-10-13 23:13:06 +0000

    Etap II prawie zaimplementowany

---
+++ v1.3/background.cpp
@@ -0,0 +1,50 @@
+#include "background.h"
+background::background(){}
+background::background(int _width, int _height){
+	width=_width;
+	height=_height;
+	content=new vector<blok>(width*height);
+		   }
+blok& background::operator()(int x, int y)
+{
+return content->at(y+x*width);
+}
+void background::insertAt(int x,int y, blok & blk){
+	vector<blok>::iterator it;
+
+	it = content->begin();
+	for(int i=0;i<x*width+y;i++)
+		++it;
+	content->insert(it,blk);
+}
+Mat & background::devectorize(){
+	Mat *tmp=new Mat(height*content->at(0).getSqrtSize(),width*content->at(0).getSqrtSize(),CV_8UC1,Scalar(0));
+	//cout<<content->at(0).getSize();
+	//cout<<"POJ:"<<content->capacity();
+	//cout<<width*height;
+	//for(unsigned int a=0;a<width*height;a++)
+	vector<blok>::iterator it=content->begin();
+		int size=content->at(0).getSqrtSize();
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			
+			if(it->getSize())
+			it->devectorize().copyTo((*tmp)(Rect(j*size, i*size, size, size)));
+			it++;
+			}
+		
+	}
+		//for(int i=0;i<content->at(0).getSqrtSize();i++)
+			//for(int j=0;j<content->at(0).getSqrtSize();j++)
+				//tmp->at<uchar>(i+a/width*content->at(0).getSqrtSize(),j+a%width)=content->at(a).devectorize().at<uchar>(i,j);
+return *tmp;
+}
+void background::reserve(int n){
+	*content=vector<blok>(n);
+}
+void background::setWidth(int _width){
+	width=_width;
+}
+void background::setHeight(int _height){
+	height=_height;
+}	
---
+++ v1.3/background.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <vector>
+#include <iostream>
+#include <windows.h>
+#include <ctype.h>
+#include <time.h>
+#include <iomanip>
+#include <deque>
+#include <math.h>
+#include "blok.h"
+using namespace cv;
+using namespace std;
+class background{
+private:
+	std::vector<blok>* content;
+	int width, height;
+public:
+	background();
+	background(int,int);
+	Mat & devectorize();
+	blok & operator()(int, int);
+	void insertAt(int, int,blok&);
+	void reserve(int);
+	void setWidth(int);
+	void setHeight(int);
+
+};
