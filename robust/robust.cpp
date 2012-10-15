2e9d5dac9649a880b973e25e38d38aed27612da4
tekiela.zbigniew <tekiela.zbigniew@gmail.com>
2012-10-15 20:54:56 +0000

    dodany test MOG'a

--- robust/robust.cpp
+++ robust/robust.cpp
@@ -1,7 +1,8 @@
 #include "opencv2/video/tracking.hpp"
+#include "opencv2/video/video.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
+#include <dos.h>
 #include <iostream>
 #include <windows.h>
 #include <ctype.h>
@@ -43,6 +44,7 @@ int size = 12;
 }*/
 
 int main(int argc, const char** argv) {
+	int method = 1;
 	string str_null("null");
 	Mat** tmp;
 
@@ -66,117 +68,178 @@ int main(int argc, const char** argv) {
 
 	cout << "szerokosc filmu: " << movie_width << "px\twysokosc filmu: "
 		<< movie_height << "px" << endl;
-	cvNamedWindow("inicjalizacja-tla", CV_WINDOW_AUTOSIZE);
-	//cvNamedWindow("kawalek", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("kolor", CV_WINDOW_AUTOSIZE);
+
 	int klatka=0;
-	while (1) {
-		klatka++;
-		if(klatka==100)
-			break;
-		cout<<"Obrobka klatki nr: "<<klatka<<endl;
-		vcap >> color;
-		if (color.empty()) {
-			cout << "ERROR: pusta ramka...\n";
-			break;
-		} //na razie ograniczanmy ilosc przechwyconych ramek
-   		//cout<<nrramki<<endl;
-		//nrramki++;
-		//if(nrramki%100!=0)
-		//{continue;}
-	//	system("Pause");
-				cvtColor(color, gray, CV_BGR2GRAY);
-				if(init)
-				{
-					::grid_height = (int) gray.rows / size;
-					::grid_width = (int) gray.cols / size;
-					grd.setHeight(grid_height);
-					grd.setWidth(grid_width);
-					grd.reserve(grid_height*grid_width);
-					bg.setHeight(grid_height);
-					bg.setWidth(grid_width);
-					bg.reserve(grid_height*grid_width);
-					cout<<grd.getWidth()<<" "<<grd.getHeight();
-					system("PAUSE");
-					init=false;
-				}
-				
-		imshow("kolor",color);
-		imshow("inicjalizacja-tla", gray);
-		char cc = (char) waitKey(10);
-		tmp = grid_cut(gray, size);
-		for(int i=0;i<::grid_width;i++)
-			for (int j=0;j<::grid_height;j++)
-			{	
-				bool flaga=false;
-				vector<blok> wektor=grd(j,i);
-				vector<blok>::iterator it;
-				blok b=blok(tmp[j][i],size);
-				//imshow("kawalek",b.devectorize());
-				//char kawalek_char=(char) waitKey(10);
-				for(it=grd(j,i).begin();it!=grd(j,i).end();it++)
-				{
-					//cout<<i<<","<<j<<"przed sim";
-					//system("PAUSE");
-					if(it->similar(b,T1,T2))
+
+	switch(method){
+
+
+	case 0:
+		cvNamedWindow("inicjalizacja-tla", CV_WINDOW_AUTOSIZE);
+		//cvNamedWindow("kawalek", CV_WINDOW_AUTOSIZE);
+		cvNamedWindow("kolor", CV_WINDOW_AUTOSIZE);
+		while (1) {
+			klatka++;
+			if(klatka==100)
+				break;
+			cout<<"Obrobka klatki nr: "<<klatka<<endl;
+			vcap >> color;
+			if (color.empty()) {
+				cout << "ERROR: pusta ramka...\n";
+				break;
+			} //na razie ograniczanmy ilosc przechwyconych ramek
+   			//cout<<nrramki<<endl;
+			//nrramki++;
+			//if(nrramki%100!=0)
+			//{continue;}
+		//	system("Pause");
+					cvtColor(color, gray, CV_BGR2GRAY);
+					if(init)
 					{
-						flaga=true;
-						it->update(b);
-						//cout<<"similar!";
-						//cout<<it->getWeight();
-						
+						::grid_height = (int) gray.rows / size;
+						::grid_width = (int) gray.cols / size;
+						grd.setHeight(grid_height);
+						grd.setWidth(grid_width);
+						grd.reserve(grid_height*grid_width);
+						bg.setHeight(grid_height);
+						bg.setWidth(grid_width);
+						bg.reserve(grid_height*grid_width);
+						cout<<grd.getWidth()<<" "<<grd.getHeight();
+						system("PAUSE");
+						init=false;
 					}
+				
+			imshow("kolor",color);
+			imshow("inicjalizacja-tla", gray);
+			char cc = (char) waitKey(10);
+			tmp = grid_cut(gray, size);
+			for(int i=0;i<::grid_width;i++)
+				for (int j=0;j<::grid_height;j++)
+				{	
+					bool flaga=false;
+					vector<blok> wektor=grd(j,i);
+					vector<blok>::iterator it;
+					blok b=blok(tmp[j][i],size);
+					//imshow("kawalek",b.devectorize());
+					//char kawalek_char=(char) waitKey(10);
+					for(it=grd(j,i).begin();it!=grd(j,i).end();it++)
+					{
+						//cout<<i<<","<<j<<"przed sim";
+						//system("PAUSE");
+						if(it->similar(b,T1,T2))
+						{
+							flaga=true;
+							it->update(b);
+							//cout<<"similar!";
+							//cout<<it->getWeight();
+						
+						}
 
+					}
+					if(flaga==false){
+						blok c(tmp[j][i],size);
+						grd.insertAt(i,j,(blok(tmp[j][i],size)));
+					}
 				}
-				if(flaga==false){
-					blok c(tmp[j][i],size);
-					grd.insertAt(i,j,(blok(tmp[j][i],size)));
-				}
-			}
-		// cout << x <<","<<y<<endl;*/
-		//blok blk(tmp[y][x],size);
-	/*	if(blk.getSize())
-		{
-		Mat kawalek2=blk.devectorize();
-		cout<<kawalek2;
-		imshow("kawalek", tmp[y][x]);
-		imshow("kawalek2", kawalek2);
-		}*/
-		/*
-		grd.insertAt(2,3,blk);
-		cout<<"size:"<<grd(3,2).at(0).getSize();
-		if(grd(3,2).at(0).getSize())
+			// cout << x <<","<<y<<endl;*/
+			//blok blk(tmp[y][x],size);
+		/*	if(blk.getSize())
 			{
-				blok blk4=grd(3,2).at(0);
-				imshow("kawalek4",blk4.devectorize());
-				cout<<"corr:";
-				cout<<blk.corelation(blk4)<<endl;
-				cout<<"mad:"<<blk.mad(blk4);
+			Mat kawalek2=blk.devectorize();
+			cout<<kawalek2;
+			imshow("kawalek", tmp[y][x]);
+			imshow("kawalek2", kawalek2);
 			}*/
+			/*
+			grd.insertAt(2,3,blk);
+			cout<<"size:"<<grd(3,2).at(0).getSize();
+			if(grd(3,2).at(0).getSize())
+				{
+					blok blk4=grd(3,2).at(0);
+					imshow("kawalek4",blk4.devectorize());
+					cout<<"corr:";
+					cout<<blk.corelation(blk4)<<endl;
+					cout<<"mad:"<<blk.mad(blk4);
+				}*/
 		
-		//if (cc == 27)
-			//break;
+			//if (cc == 27)
+				//break;
 		
+		}
+
+		grd.fix();
+		for(int i=0;i<::grid_height;i++)
+		{	
+			cout<<endl;
+			for (int j=0;j<::grid_width;j++)
+			{
+				cout<<grd(i,j).size()<<","<<grd(i,j).at(0).getWeight()<<" ";
+				Mat tmp=grd(i,j).at(0).devectorize();
+				cvNamedWindow("test");
+				imshow("test",tmp);
+				char cdd=(char) waitKey(10);
+				//system("PAUSE");
+				bg.insertAt(i,j,grd(i,j).at(0)); //testowe,moze krzaczyc
+			}
+		}
+		cvNamedWindow("wynik");
+	
+		imshow("wynik",bg.devectorize()); //jeszcze nie dziala
+
+		break;
+
+	case 1:
+
+		namedWindow("kolor", CV_WINDOW_NORMAL);
+		namedWindow("maska pierwszego planu", CV_WINDOW_NORMAL);
+		namedWindow("pierwszy plan", CV_WINDOW_NORMAL);
+		
+		BackgroundSubtractorMOG2 bg_model;//(100, 3, 0.3, 5);
+
+		Mat img,fgmask, fgimg;
+
+		while (1) {
+			klatka++;
+			//if(klatka==100)
+				//break;
+
+			//nie dzia³a dla szarego obrazu
+			vcap >> img;
+			if( img.empty() )
+				break;
+
+			if( fgimg.empty() )
+				fgimg.create(img.size(), img.type());
+
+			//aktualizacja modelu
+			bg_model(img, fgmask, -1);
+
+			//zerowanie obrazka pierwszego planu
+			fgimg = Scalar::all(0);
+			img.copyTo(fgimg, fgmask);
+
+			//Mat bgimg;
+			//bg_model.getBackgroundImage(bgimg);
+
+			imshow("kolor", img);
+			imshow("maska pierwszego planu", fgmask);
+			imshow("pierwszy plan", fgimg);
+			//if(!bgimg.empty())
+			//	imshow("mean background image", bgimg );
+
+			char k = (char)waitKey(10);
+
+		}
+
+		namedWindow("wynik", CV_WINDOW_NORMAL);
+		Mat bgimg;
+		bg_model.getBackgroundImage(bgimg);
+		if(!bgimg.empty())
+			imshow("wynik", bgimg );
+		break;
+
 	}
 
-	grd.fix();
-	for(int i=0;i<::grid_height;i++)
-	{	
-		cout<<endl;
-		for (int j=0;j<::grid_width;j++)
-		{
-			cout<<grd(i,j).size()<<","<<grd(i,j).at(0).getWeight()<<" ";
-			Mat tmp=grd(i,j).at(0).devectorize();
-			cvNamedWindow("test");
-			imshow("test",tmp);
-			char cdd=(char) waitKey(10);
-			//system("PAUSE");
-			bg.insertAt(i,j,grd(i,j).at(0)); //testowe,moze krzaczyc
-		}
-	}
-	cvNamedWindow("wynik");
-	
-	imshow("wynik",bg.devectorize()); //jeszcze nie dziala
 	char wynik_char=(char) waitKey(10);
 	system("PAUSE");	
 	clock_t after = clock();
